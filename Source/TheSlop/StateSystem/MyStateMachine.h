7f7a7c6737638e52b63b191060f097db2371ede5
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2025-12-10 18:45:04 +0100

    Added a start to a state system

---
+++ Source/TheSlop/StateSystem/MyStateMachine.h
@@ -0,0 +1,16 @@
+﻿
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include <UObject/Object.h>
+#include "MyStateMachine.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THESLOP_API UMyStateMachine : public UObject
+{
+	GENERATED_BODY()
+};
---
+++ Source/TheSlop/StateSystem/StateMachine.cpp
@@ -0,0 +1,2 @@
+﻿#include "C:\HKU\2.2 ExpandYourself\ExpandYourself\SideProjects\TheSlop\Intermediate\Build\Win64\x64\TheSlopEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.h"
+#include "StateMachine.h"
